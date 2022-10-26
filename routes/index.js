b311eb2b487cd0bc0eeff8d2fddef39f24bff2b5
coltonkramer <77127051+coltonkramer@users.noreply.github.com>
2022-10-26 16:31:34 -0400

    possibly fixed front end path issues

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const cors = require('cors');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/frontend/index.html'));
+});
+router.use('/swagger', require('./swagger'));
 
-router.use('/', require('./swagger'));
 router.use('/routes', require('./routes'));
 module.exports = router;
